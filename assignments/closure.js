882e511d30d2876585d934b927586fe728917d70
Elizabeth Johnson <lambda_school_loaner_117@Lambda-School-Loaner-117s-MacBook-Pro.local>
2019-05-25 20:06:39 -0400

    working on closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -57,7 +57,18 @@ console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  const counter = 0;
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  increment => {
+    increase = counter++;
+  }
+return increment;
+  decrement => {
+    decrease = counter--;
+  }
+ return decrement;
 };
+
+counterFactory(increment, decrement);
