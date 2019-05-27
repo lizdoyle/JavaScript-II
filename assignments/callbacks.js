9d25014b4d896d0c601cb2b11083f8e94e30f41a
Elizabeth Johnson <lambda_school_loaner_117@Lambda-School-Loaner-117s-MacBook-Pro.local>
2019-05-27 16:36:39 -0400

    almost done wth callbacks :)

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -10,20 +10,20 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   // Potential Solution:
 
   // Higher order function using "cb" as the call back
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
-  // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
+  // function firstItem(arr, cb) {
+  //   return cb(arr[0]);
+  // }
+  // // Function invocation 
+  // firstItem(items, function(first) {
+  //   console.log(first)
+  // });
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr.length);
 }
-
+// complete
 getLength(items, function(getLength){
   console.log(getLength)
 });
@@ -32,34 +32,39 @@ getLength(items, function(getLength){
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   
-  return cb()
+  return cb(arr[arr.length - 1])
 }
-
-// last(items, function(last){
-//   console.log(last)
-// });
+// complete
+last(items, function(last){
+  console.log(last)
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  
-  return cb();
+  return  cb(x + y);
 }
-
-// sumNums(sum, function(sumNums){
-//   console.log(sumNums(1, 9))
-// });
+// complete
+sumNums(25, 10, function(sumNums){
+  console.log(sumNums)
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+return cb(x * y);
 }
-
+// complete
+multiplyNums(3, 4, function(multiplyNums){
+  console.log(multiplyNums)
+});
 
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
-
+// contains(items, function(x){
+//   console.log(contains)
+// });
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
